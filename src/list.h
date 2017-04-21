a1b4cdcacd8392ee180de0951263b0d5372fa7ce
Christian Heckendorf <heckendorfc@gmail.com>
2017-04-21 11:19:37 -0400

    Add missing macro from last commit.

--- src/list.h
+++ src/list.h
@@ -3,6 +3,7 @@
 
 #define NEXT_NODE(node) node->next
 #define FINDTAILNODE(x) if(x)while(x->next)x=x->next;
+#define ADDNODE(x,t) if(x){t=x; x=malloc(sizeof(*x)); x->next=t;}
 #define ADDNODEAFTER(x,t) if(x){t=x->next; x->next=malloc(sizeof(*t)); x->next->next=t;}
 
 #endif
